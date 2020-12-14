fcdb7bc39de9f2f4316c9fec06d0243dcd060818
mgarrity23 <mgarrit8@villanova.edu>
2020-12-14 16:08:22 -0500

    New files

---
+++ Beginner/console_adv/adv_console.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+play = True
+while(play):
+    print('Welcome to my command line adventure game:')
+    start_input = input('Enter \'Start\' to begin!')
+    start_input = start_input.lower()
+    if start_input == 'start':
+        pass
+    else:
+        break
