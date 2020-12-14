5e46d6d0a19a29bf8e78f27f1cf3c19876bb5229
mgarrity23 <mgarrit8@villanova.edu>
2020-12-14 16:28:32 -0500

    commit new changes

--- Beginner/console_adv/adv_console.py
+++ Beginner/console_adv/adv_console.py
@@ -2,10 +2,12 @@ import numpy as np
 
 play = True
 while(play):
-    print('Welcome to my command line adventure game:')
-    start_input = input('Enter \'Start\' to begin!')
+    print('Welcome to my command line adventure game!')
+    start_input = input('Enter \'Start\' to begin: ')
     start_input = start_input.lower()
     if start_input == 'start':
+        rooms = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        print(rooms)
         pass
     else:
         break
